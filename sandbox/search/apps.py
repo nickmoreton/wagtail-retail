03ff6a1a70cbecb5b536e003a3b647ce47a4378c
Nick Moreton <nickmoreton@me.com>
2023-05-06 15:19:11 +0100

    Remove file not required at the moment

--- sandbox/search/apps.py
+++ sandbox/search/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShopConfig(AppConfig):
+class BasketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sandbox.shop'
+    name = 'sandbox.search'
